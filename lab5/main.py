a76cccdeb9a276062015ecff048075b90f5bfc43
Denys Vasylyshyn <den.makakass@gmail.com>
2023-12-14 02:03:34 +0200

    lab5 fix

--- lab5/main.py
+++ lab5/main.py
@@ -1,6 +1,6 @@
 from dsa import generate_dsa, DSAKeys, dsa_sign, dsa_verify
 from rsa import generate_rsa, RSAKeys, rsa_encrypt, rsa_decrypt
-from utils import hash_function, random_divider
+from utils import random_divider
 from uuid import uuid4, UUID
 
 
@@ -16,10 +16,10 @@ class Voter:
         self.id = uuid4()
         self.dsa_keys = generate_dsa()
 
-    def create_vote_message(self, candidate: int, *keys: RSAKeys.public_key):
+    def create_vote_message(self, candidate: int, pub_key: RSAKeys.public_key):
         div1, div2 = random_divider(candidate)
-        encrypted_msg1 = rsa_encrypt(div1, keys[0])
-        encrypted_msg2 = rsa_encrypt(div2, keys[1])
+        encrypted_msg1 = rsa_encrypt(div1, pub_key)
+        encrypted_msg2 = rsa_encrypt(div2, pub_key)
         signature1 = dsa_sign(encrypted_msg1, self.dsa_keys.private)
         signature2 = dsa_sign(encrypted_msg2, self.dsa_keys.private)
         return signature1, encrypted_msg1, signature2, encrypted_msg2, self.id
@@ -28,7 +28,6 @@ class Voter:
 class EC:
     def __init__(self, anon_voters: dict[UUID, DSAKeys.public], ph: str):
         self.voters_to_vote = anon_voters
-        self.rsa_keys = generate_rsa()
         self.result_dict = dict()
         self.trace_phrase = ph
 
@@ -45,8 +44,7 @@ class EC:
         except ValueError as e:
             print(self.trace_phrase+":", voter_id, e)
             return
-        decrypted_divider = rsa_decrypt(verified_msg, self.rsa_keys.private_key)
-        self.result_dict[voter_id] = decrypted_divider
+        self.result_dict[voter_id] = verified_msg
         del self.voters_to_vote[voter_id]
 
     def results(self):
@@ -65,6 +63,7 @@ class CentralEC:
 
     def __init__(self, voters: list[Voter]):
         self.voters = voters
+        self.rsa_keys = generate_rsa()
 
     def get_anon_voters(self):
         return dict((voter.id, voter.dsa_keys.public) for voter in self.voters)
@@ -73,6 +72,7 @@ class CentralEC:
         result = {k: d1.get(k, 0) * d2.get(k, 0) for k in set(d1) & set(d2)}
         for voter_id, candidate in result.items():
             voter_name = (v.name for v in self.voters if v.id == voter_id).__next__()
+            candidate = rsa_decrypt(candidate, self.rsa_keys.private_key)
             if any(c.id == candidate for c in CentralEC.candidates):
                 print(f'Виборець {voter_id} ({voter_name}) проголосував за кандидата {candidate}')
             else:
@@ -87,35 +87,34 @@ def main():
     voters_list = [v1, v2, v3, v4]
 
     cec = CentralEC(voters_list)
-    ec1 = EC(cec.get_anon_voters(), "ВД1")
-    ec2 = EC(cec.get_anon_voters(), "ВД2")
-    keys = (ec1.rsa_keys.public_key, ec2.rsa_keys.public_key)
+    ec1 = EC(cec.get_anon_voters(), "ВК1")
+    ec2 = EC(cec.get_anon_voters(), "ВК2")
+    pub_key = cec.rsa_keys.public_key
 
     # v1
-    s1, msg1, s2, msg2, v_id = v1.create_vote_message(8, *keys)
+    s1, msg1, s2, msg2, v_id = v1.create_vote_message(8, pub_key)
     ec1.register_vote(s1, msg1, v_id)
     ec2.register_vote(s2, msg2, v_id)
     # v2
-    s1, msg1, s2, msg2, v_id = v2.create_vote_message(9, *keys)
+    s1, msg1, s2, msg2, v_id = v2.create_vote_message(9, pub_key)
     ec1.register_vote(s1, msg1, v_id)
     ec2.register_vote(s2, msg2, v_id)
     # v2 голосує повторно
-    s1, msg1, s2, msg2, v_id = v2.create_vote_message(9, *keys)
-    ec1.register_vote(s1, msg1, v_id)
+    s1, msg1, s2, msg2, v_id = v2.create_vote_message(9, pub_key)
+    # ec1.register_vote(s1, msg1, v_id)
     ec2.register_vote(s2, msg2, v_id)
     # v3 голосує за неіснуючого кандидата (невірний бюлетень)
-    s1, msg1, s2, msg2, v_id = v3.create_vote_message(12, *keys)
+    s1, msg1, s2, msg2, v_id = v3.create_vote_message(12, pub_key)
     ec1.register_vote(s1, msg1, v_id)
     ec2.register_vote(s2, msg2, v_id)
     # v4 намагається проголосувати з невірним signature
-    s1, msg1, s2, msg2, v_id = v4.create_vote_message(9, *keys)
+    s1, msg1, s2, msg2, v_id = v4.create_vote_message(9, pub_key)
     ec1.register_vote(b"random", msg1, v_id)
     ec2.register_vote(s2, msg2, v_id)
     # v5 не зареєстрований
-    s1, msg1, s2, msg2, v_id = v5.create_vote_message(9, *keys)
+    s1, msg1, s2, msg2, v_id = v5.create_vote_message(9, pub_key)
     ec1.register_vote(s1, msg1, v_id)
     ec2.register_vote(s2, msg2, v_id)
-
 
     print("\n_____ВК1 публікує результати_____")
     res1 = ec1.results()
